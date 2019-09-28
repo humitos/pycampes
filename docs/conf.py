faf7547575741310e252798996c337d2c922da08
Manuel Kaufmann <humitos@gmail.com>
2019-09-28 12:10:34 +0200

    set master doc

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,8 @@ author = 'Manuel Kaufmann'
 # The full version, including alpha/beta/rc tags
 release = '2020'
 
+master_doc = 'index'
+
 
 # -- General configuration ---------------------------------------------------
 
